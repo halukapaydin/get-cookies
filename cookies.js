12bef83d42e8a72ee6fbd05e32feddd17f92d61b
haluk.apaydin <haluk.apaydin@agmlab.com>
2021-06-29 15:09:32 +0300

    browser_specific_settings

--- cookies.js
+++ cookies.js
@@ -1,48 +1,52 @@
 var cookieJson;
 
 function showCookiesForTab(tabs) {
-  //get the first tab object in the array
-  let tab = tabs.pop();
+    //get the first tab object in the array
+    let tab = tabs.pop();
 
-  //get all cookies in the domain
-  var gettingAllCookies = browser.cookies.getAll({url: tab.url});
-  gettingAllCookies.then((cookies) => {
+    //get all cookies in the domain
+    var gettingAllCookies = browser.cookies.getAll({url: tab.url});
+    gettingAllCookies.then((cookies) => {
 
-    //set the header of the panel
-    var activeTabUrl = document.getElementById('header-title');
-    var text = document.createTextNode("Cookies at: " + tab.title);
-    var cookieList = document.getElementById('cookie-list');
-    activeTabUrl.appendChild(text);
+        //set the header of the panel
+        var activeTabUrl = document.getElementById('header-title');
+        var text = document.createTextNode("Cookies at: " + tab.url);
+        var cookieList = document.getElementById('cookie-list');
+        activeTabUrl.appendChild(text);
 
-    if (cookies.length > 0) {
-      cookieJson = JSON.stringify(cookies);
-      let jsonElement = document.getElementById("json");
-      let jsonContent = document.createTextNode(cookieJson);
-      jsonElement.appendChild(jsonContent);
+        if (cookies.length > 0) {
+            cookieJson = cookiesToString(cookies);
+            let jsonElement = document.getElementById("json");
+            jsonElement.value = cookieJson;
+        } else {
+            let p = document.createElement("p");
+            let content = document.createTextNode("No cookies in this tab.");
+            let parent = cookieList.parentNode;
 
-    } else {
-      let p = document.createElement("p");
-      let content = document.createTextNode("No cookies in this tab.");
-      let parent = cookieList.parentNode;
-
-      p.appendChild(content);
-      parent.appendChild(p);
-    }
-  });
+            p.appendChild(content);
+            parent.appendChild(p);
+        }
+    });
 }
+function cookiesToString(cookies) {
+    let join = cookies.map(cookie=>{
+        return JSON.stringify(cookie)
+    });
+    return JSON.stringify(join);
+}
+
 
 //get active tab to run an callback function.
 //it sends to our callback an array of tab objects
 function getActiveTab() {
-  return browser.tabs.query({currentWindow: true, active: true});
+    return browser.tabs.query({currentWindow: true, active: true});
 }
 
 function copy() {
-  var copyText = document.getElementById("json");
-  copyText.select();
-  document.execCommand("copy");
+    var copyText = document.getElementById("json");
+    copyText.select();
+    document.execCommand("copy");
 }
 
 document.getElementById("copy").addEventListener("click", copy);
-
 getActiveTab().then(showCookiesForTab);
