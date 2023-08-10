4a610a8d3a18b7994b6bb20a79a6a507aa53e985
haluk apaydın <haluk.apaydin@agmlab.com>
2023-08-10 19:06:15 +0400

    local storage added

--- cookies.js
+++ cookies.js
@@ -1,36 +1,43 @@
-var cookieJson;
-
 function showCookiesForTab(tabs) {
     //get the first tab object in the array
     let tab = tabs.pop();
 
     //get all cookies in the domain
-    var gettingAllCookies = browser.cookies.getAll({url: tab.url});
+    const gettingAllCookies = browser.cookies.getAll({url: tab.url});
     gettingAllCookies.then((cookies) => {
-
         //set the header of the panel
-        var activeTabUrl = document.getElementById('header-title');
-        var text = document.createTextNode("Cookies at: " + tab.url);
-        var cookieList = document.getElementById('cookie-list');
+        const activeTabUrl = document.getElementById('header-title');
+        const text = document.createTextNode("Cookies at: " + tab.url);
         activeTabUrl.appendChild(text);
 
-        if (cookies.length > 0) {
+        let cookieJson = "No cookies in this tab."
+        if (cookies && cookies.length > 0) {
             cookieJson = cookiesToString(cookies);
-            let jsonElement = document.getElementById("json");
-            jsonElement.value = cookieJson;
-        } else {
-            let p = document.createElement("p");
-            let content = document.createTextNode("No cookies in this tab.");
-            let parent = cookieList.parentNode;
-
-            p.appendChild(content);
-            parent.appendChild(p);
         }
+        let cookieJsonElement = document.getElementById("cookieJson");
+        cookieJsonElement.value = cookieJson;
     });
+
+    let localStorageGet = browser.storage.local.get();
+    localStorageGet.then((items)=>{
+        let localStorageJsonElement = document.getElementById("localStorageJson");
+        localStorageJsonElement.value = JSON.stringify(items);
+    }, (error)=>{
+        let localStorageJsonElement = document.getElementById("localStorageJson");
+        localStorageJsonElement.value = JSON.stringify(error);
+    });
+
 }
 function cookiesToString(cookies) {
     let join = cookies.map(cookie=>{
-        return JSON.stringify(cookie)
+        return JSON.stringify({
+            name : cookie.name,
+            value : cookie.value,
+            path : cookie.path,
+            domain : cookie.domain,
+            isHttpOnly : cookie.isHttpOnly,
+            expiry : cookie.expiry
+        })
     });
     return JSON.stringify(join);
 }
@@ -42,11 +49,17 @@ function getActiveTab() {
     return browser.tabs.query({currentWindow: true, active: true});
 }
 
-function copy() {
-    var copyText = document.getElementById("json");
+function copyCookieJson() {
+    const copyText = document.getElementById("cookieJson");
     copyText.select();
     document.execCommand("copy");
 }
 
-document.getElementById("copy").addEventListener("click", copy);
+function copyLocalStorageJson() {
+    const copyText = document.getElementById("localStorageJson");
+    copyText.select();
+    document.execCommand("copy");
+}
+document.getElementById("copyCookieJson").addEventListener("click", copyCookieJson);
+document.getElementById("copyLocalStorageJson").addEventListener("click", copyLocalStorageJson);
 getActiveTab().then(showCookiesForTab);
